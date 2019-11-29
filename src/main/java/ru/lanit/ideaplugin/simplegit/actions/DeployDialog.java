5858de6354188e872562eefb35cad20ca2185875
shchurin-aleksandr <shchurin.aleksandr@gmail.com>
2019-11-29 18:42:20 +0400

    [+] Добавлен пример работы с GitRepository

---
+++ src/main/java/ru/lanit/ideaplugin/simplegit/actions/DeployDialog.java
@@ -0,0 +1,9 @@
+package ru.lanit.ideaplugin.simplegit.actions;
+
+import com.intellij.openapi.application.Application;
+
+public class DeployDialog {
+    public Application getSelectedItem() {
+        return null;
+    }
+}
