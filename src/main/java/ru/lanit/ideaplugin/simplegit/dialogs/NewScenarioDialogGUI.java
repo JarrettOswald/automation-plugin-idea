6f7d473d86249e2150b96450d09fd75b5f5b8b10
Shchurin Aleksandr <shchurin.aleksandr@gmail.com>
2019-12-03 03:13:59 +0400

    [/] Фикс диалога

--- src/main/java/ru/lanit/ideaplugin/simplegit/dialogs/NewScenarioDialogGUI.java
+++ src/main/java/ru/lanit/ideaplugin/simplegit/dialogs/NewScenarioDialogGUI.java
@@ -4,6 +4,22 @@ import javax.swing.*;
 
 public class NewScenarioDialogGUI extends JDialog {
     private JPanel contentPane;
+    private JTextField featureName;
+    private JTextField scenarioName;
+    private JTextField tags;
+
+    public JTextField getFeatureName() {
+        return featureName;
+    }
+
+    public JTextField getScenarioName() {
+        return scenarioName;
+    }
+
+    public JTextField getTags() {
+        return tags;
+    }
+
     public NewScenarioDialogGUI() {
     }
 }
