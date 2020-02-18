9a9d21aa852292e8278de75fbee6a16c424a05b6
Shchurin Aleksandr <shchurin.aleksandr@gmail.com>
2020-02-18 13:43:18 +0400

    [+] Добавлен метод создания нового сценария с параметрами из соответствующей формы

--- src/main/java/ru/lanit/ideaplugin/simplegit/SimpleGitProjectComponent.java
+++ src/main/java/ru/lanit/ideaplugin/simplegit/SimpleGitProjectComponent.java
@@ -3,12 +3,14 @@ package ru.lanit.ideaplugin.simplegit;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileSystem;
 import com.intellij.openapi.vfs.newvfs.RefreshSession;
+import gherkin.formatter.model.Tag;
 import org.jetbrains.annotations.NotNull;
 import ru.lanit.ideaplugin.simplegit.dialogs.newfeature.NewFeatureDialog;
 import ru.lanit.ideaplugin.simplegit.dialogs.pluginsettings.PluginSettingsDialog;
@@ -16,6 +18,11 @@ import ru.lanit.ideaplugin.simplegit.features.FeatureList;
 import ru.lanit.ideaplugin.simplegit.settings.PluginSettings;
 import ru.lanit.ideaplugin.simplegit.settings.PluginSettingsProvider;
 import ru.lanit.ideaplugin.simplegit.settings.SettingsChangeListener;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SimpleGitProjectComponent implements ProjectComponent, SettingsChangeListener {
     private final Project project;
@@ -57,8 +64,29 @@ public class SimpleGitProjectComponent implements ProjectComponent, SettingsChan
     public void createNewScenario() {
         System.out.println("Create new scenario in project " + project.getBasePath());
         NewFeatureDialog newFeatureDialog = new NewFeatureDialog(project);
+        settings.setSettingsToNewFeatureDialog(newFeatureDialog);
         newFeatureDialog.show();
-
+        if (newFeatureDialog.getExitCode() == DialogWrapper.OK_EXIT_CODE) {
+            File file = new File(getFeaturePath(), newFeatureDialog.getFeatureFilename() + ".feature");
+            try {
+                file.createNewFile();
+                FileWriter fileWriter = new FileWriter(file);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                printWriter.println("# language: ru");
+                printWriter.printf("Функция: %s\n", newFeatureDialog.getFeatureName());
+                for(Tag tag : newFeatureDialog.getFeatureTags()) {
+                    printWriter.printf("    @%s\n", tag.getName());
+                }
+                printWriter.printf("    Сценарий: %s\n", newFeatureDialog.getScenarioName());
+                printWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            VirtualFileSystem fileSystem = LocalFileSystem.getInstance();
+            VirtualFile virtualFile = fileSystem.refreshAndFindFileByPath(file.getAbsolutePath());
+            FileEditorManager.getInstance(project).openFile(virtualFile, true);
+            FeatureList.getInstance(project).updateFeaturesAndSelectByFilename(file.getAbsolutePath());
+        }
     }
 
     public void gitSynchronize(AnActionEvent event) {
@@ -76,8 +104,8 @@ public class SimpleGitProjectComponent implements ProjectComponent, SettingsChan
         pluginSettingsDialog.show();
         if (pluginSettingsDialog.getExitCode() == DialogWrapper.OK_EXIT_CODE) {
             settings.setSettingsFromDialog(pluginSettingsDialog);
-            Messages.showMessageDialog(project, "Selected feature path: " + pluginSettingsDialog.getFeaturePath(),
-                    "Information", Messages.getInformationIcon());
+//            Messages.showMessageDialog(project, "Selected feature path: " + pluginSettingsDialog.getFeaturePath(),
+//                    "Information", Messages.getInformationIcon());
         }
     }
 
